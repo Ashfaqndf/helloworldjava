8081000baa4d016b7902b4c86233277edbed1b13
Ashfaqndf <100716150+Ashfaqndf@users.noreply.github.com>
2022-06-05 16:22:55 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World from GitHub using webhooks one minute using webhooks for secondtime and fourth tim");
+    System.out.println("Hello World from GitHub using webhooks one minute using webhooks for secondtime and fourth time");
     }
 }
